ce5796636e4cac29dcb820131ba8d0b44abf141b
rmit-s3585826-fabio-monsalve <reqwest1>
2017-07-24 18:06:02 +1000

    test

--- app/src/main/java/s3585826/assignment1/Model/Friend.java
+++ app/src/main/java/s3585826/assignment1/Model/Friend.java
@@ -1,9 +1,5 @@
 package s3585826.assignment1.Model;
 
-/**
- * Created by orlandok on 24/7/17.
- */
-
 public class Friend {
 
     //test line
--- app/src/main/java/s3585826/assignment1/Model/Meeting.java
+++ app/src/main/java/s3585826/assignment1/Model/Meeting.java
@@ -1,3 +1,4 @@
 package s3585826.assignment1.Model;
+
 public class Meeting {
 }
