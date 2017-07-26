02b6d32ce581c5be55faf137085f544a0a362309
rmit-s3585826-fabio-monsalve <s3585826@student.rmit.edu.au>
2017-07-26 14:49:00 +1000

    Added tabbed activity menu and some class paramenters

--- app/src/main/java/s3585826/assignment1/Model/Friend.java
+++ app/src/main/java/s3585826/assignment1/Model/Friend.java
@@ -1,7 +1,10 @@
 package s3585826.assignment1.Model;
 
 public class Friend {
-
-    //test line
+    private String id;
+    private String name;
+    private String email;
+    //private birthday;
+    //private photo;
 
 }
--- app/src/main/java/s3585826/assignment1/Model/Meeting.java
+++ app/src/main/java/s3585826/assignment1/Model/Meeting.java
@@ -1,4 +1,11 @@
 package s3585826.assignment1.Model;
 
+import java.util.ArrayList;
+
 public class Meeting {
+    private String id;
+    private String title;
+    private String startTime;
+    private String endTime;
+    private ArrayList<Friend> invitedFriends;
 }
